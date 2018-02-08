8b71606e9e517c4c2b0537fe93f8029108f97f78
Andy Vaughn <andyv@mindtouch.com>
2018-02-08 15:53:26 -0800

    fix namespace

--- src/Mock/MockResponse.php
+++ src/Mock/MockResponse.php
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-namespace MindTouch\Http\mock;
+namespace MindTouch\Http\Mock;
 
 use MindTouch\Http\HttpPlug;
 
